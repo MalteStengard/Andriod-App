1f23d29ed07c09e8ec4144789f12ec8df2060354
martinstengard <martin.stengard@gmail.com>
2018-08-05 11:06:46 +0200

    Layout and conversion jar

--- app/src/main/java/com/example/malte/myapplication/FragmentTemperature.java
+++ app/src/main/java/com/example/malte/myapplication/FragmentTemperature.java
@@ -6,6 +6,19 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import se.martinstengard.conversions.temperature.*;
 
 /**
  * Created by Martin on 2018-07-13.
@@ -13,15 +26,68 @@ import android.view.ViewGroup;
 
 public class FragmentTemperature extends Fragment {
     View view;
+    EditText txtFrom;
+    EditText txtTo;
+    Spinner spinnerFrom;
+    Spinner spinnerTo;
+    Button btnConvert;
+    TextView lblFormula;
+    TextView lblExample;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
-                             Bundle savedInstanceState){
+                             Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_temperature,
                 container,
                 false);
+
+        txtFrom = view.findViewById(R.id.editTextFromValue);
+        txtTo = view.findViewById(R.id.editTextToValue);
+        spinnerFrom = view.findViewById(R.id.spinnerFromScale);
+        spinnerTo = view.findViewById(R.id.spinnerToScale);
+        btnConvert = view.findViewById(R.id.btnConvert);
+        lblFormula = view.findViewById(R.id.textFormula);
+        lblExample = view.findViewById(R.id.textExample);
+
+        // Set temperature scales.
+        setScaleToSpinner(spinnerFrom);
+        setScaleToSpinner(spinnerTo);
+
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(txtFrom.getText().toString().trim().equals("") ||
+                        (spinnerFrom.getSelectedItem() == spinnerTo.getSelectedItem())){
+                    return;
+                }
+                convert();
+            }
+        });
+
         return view;
+    }
+
+    private void setScaleToSpinner(Spinner dropdown) {
+        List<Scale> scales = Arrays.asList(Scale.values());
+        ArrayAdapter<Scale> dataAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, scales);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(dataAdapter);
+    }
+
+    public void convert() {
+        Request request = new Request() {{
+            fromValue = txtFrom.getText().toString();
+            fromScale = Scale.valueOf(spinnerFrom.getSelectedItem().toString());
+            toScale = Scale.valueOf(spinnerTo.getSelectedItem().toString());
+        }};
+
+        final Convert convert = new Convert();
+        final Response response = convert.run(request);
+
+        txtFrom.setText(response.fromValue);
+        txtTo.setText(response.toValue);
+        lblFormula.setText(response.formula);
+        lblExample.setText(response.calculation);
     }
 }
